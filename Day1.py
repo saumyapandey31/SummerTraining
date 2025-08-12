3c61cd4f192b88645d8c413b1c4f1ee80842195b
saumyapandey31 <saumyapandey791@gmail.com>
2025-08-12 23:53:47 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,22 @@
+#This is My First Day Training Session
+
+print("Hello World")
+num = 100
+pie = 3.14
+name = "Saumya"
+
+print(num, pie)
+
+print (''' This is my
+Multi line
+String''')
+
+print (f"My name is {name}")
+
+
+#Let's Use operators
+
+num1 = int(input("Enter First Number:- "))
+num2 = int(input("Enter Second Number:- "))
+sum = num1+num2
+print (sum)
